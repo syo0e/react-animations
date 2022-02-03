17dad3176028a4775e6c26907205fa554be87585
kimdlzp <kimdlzp@gmail.com>
2022-02-03 18:09:28 +0900

    Basic Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vh;
@@ -8,19 +9,22 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled.(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      <Box 
+        transition={{type: "spring", delay: 0.5}}
+        initial={{ scale: 0}}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 }
