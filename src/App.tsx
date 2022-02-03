f22887571fbb34fd3880a4bdcdfd944d47117223
kimdlzp <kimdlzp@gmail.com>
2022-02-03 18:28:08 +0900

    variant part one

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,29 @@ import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
-  width: 100vh;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const Box = styled.(motion.div)`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
   border-radius: 15px;
-  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
 
 function App() {
   return (
     <Wrapper>
-      <Box 
-        transition={{type: "spring", delay: 0.5}}
-        initial={{ scale: 0}}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
